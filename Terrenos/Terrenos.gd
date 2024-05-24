97c40a9938328dec801f068f0ed2e8e1708fca69
nathandrake3652 <131317866+nathandrake3652@users.noreply.github.com>
2024-05-24 17:19:54 -0400

    nuevos terrenos

--- Terrenos/Terrenos.gd
+++ Terrenos/Terrenos.gd
@@ -7,7 +7,7 @@ var velocidad = 10
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.z -= velocidad *delta
-	if position.z < -20:
+	if position.z < -45:
 		nivel.spawnescena(position.z+(nivel.amount*nivel.offset))
 		queue_free()
 		
