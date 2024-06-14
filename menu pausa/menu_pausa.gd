9f5eac2e6228b93eba4079900345068e12a51142
1nt3 <mauricio.alarcon@alumnos.ucn.cl>
2024-06-14 02:05:28 -0400

    Update menu_pausa.gd
    Se ha cambiado el método "preload" por el método "load()" para que los botones del menú principal funcionen al pulsar la opción de "salir". No sé si hay otra forma de resolver este problema.
    
    Además, se ha añadido la instrucción "Engine.time_scale = 1" (quitar modo pausa) porque al salir al menú principal y reiniciar el juego, el personaje aparecía quieto como si estuviera en modo pausa. Es una solución que se me ocurrio en el momento, desconozco si hay otra forma de evitar ese problema.

--- menu pausa/menu_pausa.gd
+++ menu pausa/menu_pausa.gd
@@ -1,11 +1,15 @@
 extends Control
 
-@onready var menu_principal = preload("res://menu principal/menu_principal.tscn") as PackedScene
+var menu_principal_scene = load("res://menu principal/menu_principal.tscn") as PackedScene
 @onready var salir = $MarginContainer/VBoxContainer/Salir as Button
 @onready var continuar = $MarginContainer/VBoxContainer/Continuar as Button
-func _ready():
+
+
+func _ready():	
 	salir.button_down.connect(on_salir_pressed)
 	#continuar.button_down.connect()
 
 func on_salir_pressed() -> void:
-	get_tree().change_scene_to_packed(menu_principal)
+	Engine.time_scale = 1
+	get_tree().change_scene_to_packed(menu_principal_scene)
+	
