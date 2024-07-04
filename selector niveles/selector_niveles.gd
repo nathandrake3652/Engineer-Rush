899d9d6e63375711ef8ec81451f4b0418efe8b9b
heuremi <cvazqueztes@gmail.com>
2024-07-03 23:18:40 -0400

    selector niveles

---
+++ selector niveles/selector_niveles.gd
@@ -0,0 +1,19 @@
+class_name selector_niveles
+extends Control
+
+@onready var nivel1 = $Button as Button
+@onready var nivel2 = $Button2 as Button
+@onready var nivel3 = $Button3 as Button
+@onready var nivel4 = $Button4 as Button
+@onready var nivel5 = $Button5 as Button
+@onready var menu_principal = $MarginContainer/HBoxContainer/VBoxContainer/Button as Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#nivel1.rect_position = Vector(100, 200)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	pass
