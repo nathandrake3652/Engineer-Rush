5083dbe87b53524da00160a21717a69433b64d24
nathandrake3652 <131317866+nathandrake3652@users.noreply.github.com>
2024-07-04 00:59:45 -0400

    score

--- Menu Muerte/menu_muerte.gd
+++ Menu Muerte/menu_muerte.gd
@@ -4,9 +4,16 @@ extends Control
 
 @onready var menu_principal = $MarginContainer/HBoxContainer/VBoxContainer/Button as Button
 @onready var ir_menu_principal = load("res://menu principal/menu_principal.tscn") as PackedScene
+@onready var label = $MarginContainer/HBoxContainer/VBoxContainer/Label
+
+var score = Variables.score
 
 func _ready():
 	menu_principal.button_down.connect(on_mp_pressed)
-
+	actualizar_label()
 func on_mp_pressed() -> void:
 	get_tree().change_scene_to_packed(ir_menu_principal)
+
+func actualizar_label():
+	label.text = "Score: " + str(score)
+	label.z_index = 1
