8764ac56414e1a039af217dc7b86d1cca7c3e8ec
nathandrake3652 <131317866+nathandrake3652@users.noreply.github.com>
2024-07-04 01:47:48 -0400

    boton combinaciones

--- menu principal/menu_principal.gd
+++ menu principal/menu_principal.gd
@@ -4,9 +4,10 @@ extends Control
 
 @onready var modo_historia = $MarginContainer/HBoxContainer/VBoxContainer/Modo_historia as Button
 @onready var modo_infinito = $MarginContainer/HBoxContainer/VBoxContainer/Modo_infinito as Button
+
 @onready var salir = $MarginContainer/HBoxContainer/VBoxContainer/Salir as Button
 @onready var start_level = preload("res://mundo.tscn") as PackedScene
-
+@onready var combinaciones = preload("res://menu principal/Tutorial_Combinaciones.tscn")
 func _ready():
 	modo_infinito.button_down.connect(on_start_pressed)
 	salir.button_down.connect(on_exit_pressed)
@@ -16,3 +17,7 @@ func on_start_pressed() -> void:
 
 func on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_packed(combinaciones)
